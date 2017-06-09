3eb83764a4aa61e0ba072eb9d7cd739b9aa063e5
Travis Taylor <codenesium@gmail.com>
2017-06-09 12:50:33 -0500

    Updated nuget to use nuspec

--- BuildNugetPackages.bat
+++ BuildNugetPackages.bat
@@ -1,3 +1,3 @@
-nuget pack CommonMVC/CommonMVC.csproj   -o  ../NugetPackages
-nuget pack CommonMVC.Interfaces/CommonMVC.Interfaces.csproj   -o  ../NugetPackages
+nuget pack CommonMVC/CommonMVC.nuspec -o  ../NugetPackages
+nuget pack CommonMVC.Interfaces/CommonMVC.Interfaces.nuspec   -o  ../NugetPackages
 
